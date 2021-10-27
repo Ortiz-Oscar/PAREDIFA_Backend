d57708f865ab6fd393b864942876d57b09ad29c3
Oscar Ortiz <fabioc.fo64@gmail.com>
2021-10-26 23:45:35 -0600

    Modularización de acceso a base de datos

--- app.js
+++ app.js
@@ -2,26 +2,12 @@ const express = require('Express');
 const { graphqlHTTP,  } = require('express-graphql');
 const { GraphQLSchema, GraphQLObjectType, GraphQLInt, GraphQLList, GraphQLString } = require('graphql');
 const {automataType} = require('./models/automata')
-let { getAutomata, listAllAutomatas } = require('./config/db');
+let { getAutomata, listAllAutomatas } = require('./config/automataCRUD');
 const app = express();
 
 const automataService = require('./routes/automataService');
 const reService = require('./routes/regularExpressionService')
 const port = 3001;
-
-const automataAux = 
-    {id: 0, alphabet:["1","0"],
-    states:[
-        {id:"1635026207553",name:"S0",coord:{x:326,y:185},radius:20,end:false,start:true}
-        ,{id:"1635026207935",name:"S1",coord:{x:700,y:105},radius:20,end:true,start:false}
-        ,{id:"1635026209193",name:"S2",coord:{x:662,y:330},radius:20,end:false,start:false}],
-    transitions:[
-    {id:"1635026217885",state_src_id:"1635026207553",state_dst_id:"1635026207553",symbols:["0"]}
-   ,{id:"1635026221915",state_src_id:"1635026207553",state_dst_id:"1635026207935",symbols:["1"]}
-   ,{id:"1635026225366",state_src_id:"1635026207935",state_dst_id:"1635026207935",symbols:["1"]}
-   ,{id:"1635026229271",state_src_id:"1635026207935",state_dst_id:"1635026209193",symbols:["0"]}
-   ,{id:"1635026251272",state_src_id:"1635026209193",state_dst_id:"1635026209193",symbols:["0","1"]}]
-};
 const automataQuery = new GraphQLObjectType({
     name:'Automata_query',
     fields:()=>({
