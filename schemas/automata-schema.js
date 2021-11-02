e4051d43e0bd37ba7ac233b69fc84cf8824dc28f
Oscar Ortiz <fabioc.fo64@gmail.com>
2021-11-01 18:58:15 -0600

    Modularización de archivos

--- schemas/automata-schema.js
+++ schemas/automata-schema.js
@@ -1,6 +1,6 @@
 const { GraphQLSchema, GraphQLObjectType, GraphQLList, GraphQLString, GraphQLNonNull, GraphQLInt, GraphQLBoolean } = require('graphql');
 const {automataType} = require('../models/automata');
-const nodemailer = require("nodemailer");
+const { sendImage } = require('../utils/sendImage')
 const { getAutomata, listAllAutomatas, saveAutomata } = require('../config/automataCRUD');
 const { inputStateType } = require('../models/inputs/stateInput')
 const { inputTransitionType } = require('../models/inputs/transitionInput');
@@ -32,50 +32,10 @@ const automataQuery = new GraphQLObjectType({
                 studentId: { type: GraphQLString },
                 studentSchedule: { type: GraphQLString },
             },
-            resolve: async(_, args) => {
-                let transporter = nodemailer.createTransport({
-                    service: "gmail",
-                    host: "smtp.gmail.com",
-                    port: 587,
-                    secure: false, // true for 465, false for other ports
-                    auth: {
-                      user: "eif400paredifag01@gmail.com", // generated ethereal user
-                      pass: "zvpanjdszgbntcfi", // generated ethereal password
-                    },
-                  });
-                
-                  // send mail with defined transport object
-                  const COURSE = {
-                    code: "400",
-                    subject: "HOMEWORK",
-                    year: "2020",
-                    cycle: "||",
-                  };
-                  const DESCRIPTION = `EIF${COURSE.code}_${COURSE.subject}_${COURSE.cycle}_${COURSE.year}_${args.studentName}_${args.studentId}_${args.studentSchedule}`;
-                  let info = await transporter.sendMail({
-                    from: 'eif400paredifag01@gmail.com', // sender address
-                    to: args.mailAddres, // reciever
-                    subject: DESCRIPTION, // Subject line
-                    html: `<h3>Greetings, ${args.studentName}</h3><br></br>
-                    <p>Hope you're doing well, this email is to inform you that you have recieved an automata  \u{270D}\u{1F4DA}</p><br></br>
-                    <br></br>
-                    <p>Regards, </p><br></br>
-                    <strong>PAREDIFA 01-10 Team</strong>
-                    `,
-                    attachments:[
-                      {
-                          filename: `${DESCRIPTION}.png`,
-                          path: args.binaryInfo
-                      }
-                    ]
-                  });
-                  transporter.close();
-                  return info.messageId
-            }
+            resolve: (_, args) => sendImage(args)
         }
-
     })
-});
+})
 const automataMutation = new GraphQLObjectType({
     name:'Automata_mutation',
     description:'Write a new automata on database',
